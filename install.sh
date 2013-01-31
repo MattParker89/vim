6c5ccd3312eccc66a9af2255deaf521cc904f612
MattParker89 <mdparker89@gmail.com>
2013-01-30 21:57:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ -d vim ]
   then
     sudo rm -r vim
 fi
-sudo git clone https://github.com/joslinm/vim.git
+sudo git clone https://github.com/MattParker89/vim.git
 if [ -d .vim ]
   then
     sudo rm -r .vim
